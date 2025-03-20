6fece6cdac56c20336189ed81b777c39b13df457
sheldy <chikibamtioma@gmail.com>
2025-03-20 18:46:20 +0100

    Split methods and types by dir.

---
+++ aiomexc/methods/__init__.py
@@ -0,0 +1,6 @@
+from .account import GetAccountInformation
+from .order import QueryOrder
+from .ticker import GetTickerPrice
+from .base import MexcMethod
+
+__all__ = ["GetAccountInformation", "QueryOrder", "GetTickerPrice", "MexcMethod"]
---
+++ aiomexc/types/__init__.py
@@ -0,0 +1,13 @@
+from .base import MexcResult, MexcType
+from .account import AccountInformation, Balance
+from .order import Order
+from .ticker import TickerPrice
+
+__all__ = [
+    "MexcResult",
+    "MexcType",
+    "AccountInformation",
+    "Balance",
+    "Order",
+    "TickerPrice",
+]
